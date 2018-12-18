8bfc3092fb69331962667b18d215cd00e663c6e4
Frederico <frederico.recsky@booking.com>
2018-12-18 07:43:16 +0100

    wip

---
+++ necrospell.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+
+var items map[string]string
+
+func http_handler( w http.ResponseWriter, r *http.Request){
+	if r.Method == "GET" {
+
+	}
+}
+
+func load_items () {
+
+	
+}
+
+func main() {
+	
+}
